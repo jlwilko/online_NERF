71afa3c19176a81dd9e135fe347410f992e41e1d
Josh Wilkinson <joshlwilkinson9@gmail.com>
2023-10-16 14:01:58 +1100

    run noisy nerf on whole lfodo dataset

--- nerf/instant-ngp/scripts/do_noisy_nerf.sh
+++ nerf/instant-ngp/scripts/do_noisy_nerf.sh
@@ -13,11 +13,52 @@ cd /volume/data/lfodo/core/seq$seq_no
 pwd
 
 # define a list of noise values to add for translational noise
-trans_noise_list="0.0 0.01 0.02 0.05 0.1 0.15 0.20 0.5"
+trans_noise_list="0.0 0.01 0.02 0.05 0.1 0.15 0.20 0.3 0.4 0.5"
 rot_noise_list="0.0"
-# trans_noise_list="0.0"
-# rot_noise_list="0.0 0.1 0.2 0.5 1 2 5 10"
+# loop over each time in the sequence
+for trans_noise in $trans_noise_list; do
+    for rot_noise in $rot_noise_list; do
+        filename="transforms_trans${trans_noise}_rot${rot_noise}"
 
+        res_dir="res_trans${trans_noise}_rot${rot_noise}"
+        mkdir -p $res_dir
+
+        # echo "Adding translational noise $noise to sequence $seq_no, writing output to $filename"
+
+        # generate the ground truth poses from colmap
+        python3 /volume/scripts/colmap2noisynerf.py --images 8/ --aabb_scale 4 --out $filename --rotation_sigma 0 --translation_sigma 0 --gt-kitti ${res_dir}/gt_extrinsics.txt
+
+        # generate the noisy poses and the transforms.json for training - OVERWRITES PREVIOUS FILE
+        python3 /volume/scripts/colmap2noisynerf.py --images 8/ --aabb_scale 4 --out $filename --rotation_sigma $rot_noise --translation_sigma $trans_noise
+        # train a nerf for this file
+        python3 /volume/scripts/noisy_pose_run_nerf.py \
+        ${filename}_train.json \
+        --n_steps 5000 \
+        --screenshot_transforms ${filename}_test.json \
+        --screenshot_dir ${res_dir}/poseopt/ \
+        --test_transforms ${filename}_test.json \
+        --train_extrinsic \
+        --extrinsics_export_path ${res_dir}/opt_extrinsics.txt \
+        --rotational_noise $rot_noise \
+        --translation_noise $trans_noise \
+        --numerical_results_path results.csv
+
+
+        python3 /volume/scripts/noisy_pose_run_nerf.py \
+        ${filename}_train.json \
+        --n_steps 5000 \
+        --screenshot_transforms ${filename}_test.json \
+        --screenshot_dir ${res_dir}/noopt/ \
+        --test_transforms ${filename}_test.json \
+        --extrinsics_export_path ${res_dir}/noisy_extrinsics.txt \
+        --rotational_noise $rot_noise \
+        --translation_noise $trans_noise \
+        --numerical_results_path results.csv
+    done 
+done
+
+trans_noise_list="0.0"
+rot_noise_list="0.0 0.1 0.2 0.5 1 1.5 2 2.5 3 3.5 4"
 # loop over each time in the sequence
 for trans_noise in $trans_noise_list; do
     for rot_noise in $rot_noise_list; do
--- nerf/instant-ngp/scripts/script_multiple_noisy_nerf.sh
+++ nerf/instant-ngp/scripts/script_multiple_noisy_nerf.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-#12 was not good 
-seq_list="6 20 33 36 43" 
+#pretty sequences
+# seq_list="6 20 33 36 43" 
+# all trained sequences
+# seq_list="2 3 4 5 6 9 10 11 12 13 14 16 17 18 19 20 21 22 23 24 25 26 27 31 32 33 34 35 36 37 38 39 40 41 43"
+seq_list="37 38 39 40 41"
+#still to do  
+
 
 for i in $seq_list; do
     echo "Processing sequence $i"
